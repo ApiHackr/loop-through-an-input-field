306607e0e0ca410205ff5cb55d79fbf908b477f2
ApiHackr <elz.eth.hacker@gmail.com>
2026-05-02 09:43:19 +0200

    #

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const textInputField = document.getElementById('text-input');
+const startBtn = document.getElementById('start-btn');
+const outputField = document.getElementById('output');
+
+let textArr = '';
+startBtn.addEventListener('click', () => {
+    if (textInputField.value) {
+        for (let char of textInputField.value) {
+            textArr += char;
+            outputField.value += textArr + '\n';
+        }
+    } else {
+        outputField.value = 'Please write something in the text inputfield';
+        outputField.style.color = 'red';
+    }
+})
